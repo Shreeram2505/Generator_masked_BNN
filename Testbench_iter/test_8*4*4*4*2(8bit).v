2d4f2423f8652d010e533693f8efcf2bb13442af
Shreeram Marballi <shreeram.marballi@gmail.com>
2025-08-28 02:24:32 +0530

    Update and rename test_8*4*4*4*2(3bit).v to test_8*4*4*4*2(8bit).v

--- Testbench_iter/test_8*4*4*4*2(8bit).v
+++ Testbench_iter/test_8*4*4*4*2(8bit).v
@@ -7,14 +7,14 @@ module tb_iterative_controller;
   wire        done;
 
   // Layer (m1) inputs
-  reg  [2:0]  inputs0_1, inputs1_1, inputs2_1, inputs3_1;
-  reg  [2:0]  inputs4_1, inputs5_1, inputs6_1, inputs7_1;
+  reg  [7:0]  inputs0_1, inputs1_1, inputs2_1, inputs3_1;
+  reg  [7:0]  inputs4_1, inputs5_1, inputs6_1, inputs7_1;
   reg  [7:0]  w1_0_1, w1_1_1, w2_0_1, w2_1_1;
   reg  [7:0]  w3_0_1, w3_1_1, w4_0_1, w4_1_1;
-  reg  [5:0]  b1_1, b2_1, b3_1, b4_1;
-  reg  [5:0]  b1_2, b2_2, b3_2, b4_2;
-  reg  [5:0]  b1_3, b2_3, b3_3, b4_3;
-  reg  [5:0]  b1_4, b2_4, b3_4, b4_4;
+  reg  [10:0]  b1_1, b2_1, b3_1, b4_1;
+  reg  [10:0]  b1_2, b2_2, b3_2, b4_2;
+  reg  [10:0]  b1_3, b2_3, b3_3, b4_3;
+  reg  [10:0]  b1_4, b2_4, b3_4, b4_4;
 
   // Share (m2) inputs
   reg  [7:0]  w1_0_2, w1_1_2, w2_0_2, w2_1_2;
@@ -25,14 +25,14 @@ module tb_iterative_controller;
   reg  [7:0]  w3_0_4, w3_1_4, w4_0_4, w4_1_4;
 
   // Share (m2) outputs
-  wire [2:0]  act0_0_0_r, act0_0_1_r, act0_0_2_r, act0_0_3_r, act0_0_4_r, act0_0_5_r, act0_0_6_r, act0_0_7_r;
-  wire [2:0]  act0_1_0_r, act0_1_1_r, act0_1_2_r, act0_1_3_r, act0_1_4_r, act0_1_5_r, act0_1_6_r, act0_1_7_r;
-  wire [2:0]  act1_0_0_r, act1_0_1_r, act1_0_2_r, act1_0_3_r, act1_0_4_r, act1_0_5_r, act1_0_6_r, act1_0_7_r;
-  wire [2:0]  act1_1_0_r, act1_1_1_r, act1_1_2_r, act1_1_3_r, act1_1_4_r, act1_1_5_r, act1_1_6_r, act1_1_7_r;
-  wire [2:0]  act2_0_0_r, act2_0_1_r, act2_0_2_r, act2_0_3_r, act2_0_4_r, act2_0_5_r, act2_0_6_r, act2_0_7_r;
-  wire [2:0]  act2_1_0_r, act2_1_1_r, act2_1_2_r, act2_1_3_r, act2_1_4_r, act2_1_5_r, act2_1_6_r, act2_1_7_r;
-  wire [2:0]  act3_0_0_r, act3_0_1_r, act3_0_2_r, act3_0_3_r, act3_0_4_r, act3_0_5_r, act3_0_6_r, act3_0_7_r;
-  wire [2:0]  act3_1_0_r, act3_1_1_r, act3_1_2_r, act3_1_3_r, act3_1_4_r, act3_1_5_r, act3_1_6_r, act3_1_7_r;
+  wire [7:0]  act0_0_0_r, act0_0_1_r, act0_0_2_r, act0_0_3_r, act0_0_4_r, act0_0_5_r, act0_0_6_r, act0_0_7_r;
+  wire [7:0]  act0_1_0_r, act0_1_1_r, act0_1_2_r, act0_1_3_r, act0_1_4_r, act0_1_5_r, act0_1_6_r, act0_1_7_r;
+  wire [7:0]  act1_0_0_r, act1_0_1_r, act1_0_2_r, act1_0_3_r, act1_0_4_r, act1_0_5_r, act1_0_6_r, act1_0_7_r;
+  wire [7:0]  act1_1_0_r, act1_1_1_r, act1_1_2_r, act1_1_3_r, act1_1_4_r, act1_1_5_r, act1_1_6_r, act1_1_7_r;
+  wire [7:0]  act2_0_0_r, act2_0_1_r, act2_0_2_r, act2_0_3_r, act2_0_4_r, act2_0_5_r, act2_0_6_r, act2_0_7_r;
+  wire [7:0]  act2_1_0_r, act2_1_1_r, act2_1_2_r, act2_1_3_r, act2_1_4_r, act2_1_5_r, act2_1_6_r, act2_1_7_r;
+  wire [7:0]  act3_0_0_r, act3_0_1_r, act3_0_2_r, act3_0_3_r, act3_0_4_r, act3_0_5_r, act3_0_6_r, act3_0_7_r;
+  wire [7:0]  act3_1_0_r, act3_1_1_r, act3_1_2_r, act3_1_3_r, act3_1_4_r, act3_1_5_r, act3_1_6_r, act3_1_7_r;
 
   // Device Under Test
   iterative_controller dut (
@@ -93,8 +93,8 @@ module tb_iterative_controller;
     
 
     // Initialize static inputs
-    inputs0_1 = 3'd2; inputs1_1 = 3'd2; inputs2_1 = 3'd3; inputs3_1 = 3'd3;
-    inputs4_1 = 3'd5; inputs5_1 = 3'd6; inputs6_1 = 3'd7; inputs7_1 = 3'd6;
+    inputs0_1 = 7'd2; inputs1_1 = 7'd2; inputs2_1 = 7'd3; inputs3_1 = 7'd3;
+    inputs4_1 = 7'd5; inputs5_1 = 7'd6; inputs6_1 = 7'd7; inputs7_1 = 7'd6;
 
     {w1_0_1,w2_0_1,w3_0_1,w4_0_1} = {4{8'h0F}};
     {w1_0_2,w2_0_2,w3_0_2,w4_0_2} = {4{8'hF0}};
@@ -108,10 +108,10 @@ module tb_iterative_controller;
     {w1_1_4,w2_1_4,w3_1_4,w4_1_4} = {4{8'hAA}};
 
 
-    {b1_1,b2_1,b3_1,b4_1}         = {4{6'd0}};
-    {b1_2,b2_2,b3_2,b4_2}         = {4{6'd1}};
-    {b1_3,b2_3,b3_3,b4_3}         = {4{6'd0}};
-    {b1_4,b2_4,b3_4,b4_4}         = {{6'd2},{6'd0},{6'd0},{6'd0}};
+    {b1_1,b2_1,b3_1,b4_1}         = {4{11'd0}};
+    {b1_2,b2_2,b3_2,b4_2}         = {4{11'd1}};
+    {b1_3,b2_3,b3_3,b4_3}         = {4{11'd0}};
+    {b1_4,b2_4,b3_4,b4_4}         = {{11'd2},{11'd0},{11'd0},{11'd0}};
 
     // Pulse start
     #20 start = 1;
