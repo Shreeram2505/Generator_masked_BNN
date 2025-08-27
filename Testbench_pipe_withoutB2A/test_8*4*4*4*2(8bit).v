aec992dd441bcd80fcc1491c18150f92cd2a02f8
Shreeram Marballi <shreeram.marballi@gmail.com>
2025-08-27 23:14:56 +0530

    Update and rename test_8*4*4*4*2(3bit).v to test_8*4*4*4*2(8bit).v

--- Testbench_pipe_withoutB2A/test_8*4*4*4*2(8bit).v
+++ Testbench_pipe_withoutB2A/test_8*4*4*4*2(8bit).v
@@ -1,13 +1,15 @@
 `timescale 1ns/1ps
 `default_nettype none
 
+`include "T1.V"
+
 module connector_tb;
   // Layer‑1 inputs
-  reg  [2:0] inputs0_1, inputs1_1, inputs2_1, inputs3_1;
-  reg  [2:0] inputs4_1, inputs5_1, inputs6_1, inputs7_1;
+  reg  [7:0] inputs0_1, inputs1_1, inputs2_1, inputs3_1;
+  reg  [7:0] inputs4_1, inputs5_1, inputs6_1, inputs7_1;
   reg  [7:0] w1_0_1, w1_1_1, w2_0_1, w2_1_1;
   reg  [7:0] w3_0_1, w3_1_1, w4_0_1, w4_1_1;
-  reg  [5:0] b1_1, b2_1, b3_1, b4_1;
+  reg  [10:0] b1_1, b2_1, b3_1, b4_1;
 
   // Layer‑2 weights & biases
   reg  [7:0] w1_0_2, w1_1_2, w2_0_2, w2_1_2;
@@ -63,8 +65,8 @@ module connector_tb;
     $dumpfile("connector_tb.vcd");
     $dumpvars(0, connector_tb);
 
-    inputs0_1 = 3'd2; inputs1_1 = 3'd2; inputs2_1 = 3'd3; inputs3_1 = 3'd3;
-    inputs4_1 = 3'd5; inputs5_1 = 3'd6; inputs6_1 = 3'd7; inputs7_1 = 3'd6;
+    inputs0_1 = 7'd2; inputs1_1 = 7'd2; inputs2_1 = 7'd3; inputs3_1 = 7'd3;
+    inputs4_1 = 7'd5; inputs5_1 = 7'd6; inputs6_1 = 7'd7; inputs7_1 = 7'd6;
     {w1_0_1,w2_0_1,w3_0_1,w4_0_1} = {4{8'h0F}};
     {w1_0_2,w2_0_2,w3_0_2,w4_0_2} = {4{8'hF0}};
     {w1_0_3,w2_0_3,w3_0_3,w4_0_3} = {4{8'hFF}};
@@ -74,7 +76,7 @@ module connector_tb;
     {w1_1_3, w2_1_3, w3_1_3, w4_1_3} = {4{8'hF0}};
 
 
-    {b1_1,b2_1,b3_1,b4_1}         = {4{6'd0}};
+    {b1_1,b2_1,b3_1,b4_1}         = {4{10'd0}};
     {b1_2,b2_2,b3_2,b4_2}         = {4{3'd1}};
     {b1_3,b2_3,b3_3,b4_3}         = {4{3'd0}};
 
